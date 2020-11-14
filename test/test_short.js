059b15d54a2696556b664aef9da666103a5c8ab9
Julieth Jeraldine Cordoba Alvear <cordoba@Cordoba-COL.RCC.local>
2020-11-14 18:08:43 -0500

    test for Short-Term (hourly) Parking

--- test/test_short.js
+++ test/test_short.js
@@ -2,42 +2,56 @@ const assert = require("chai").assert;
 var index = require("../index")
 
 describe("Test for Short-Term (hourly) Parking", () => {
-    it("Test 2 hours cost is $4.00", async ()=> {
+    it("Test Fist hour cost is $2.00", async ()=> {
         // arrange
         let parkType = "Short";
         let startDate = "11/14/2020";
-        let leavingDate = "11/16/2020";
+        let leavingDate = "11/14/2020";
         let startTime = "0:00";
-        let leavingTime = "02:00";
-        let expectedCost = "$ 4.00";
+        let leavingTime = "01:00";
+        let expectedCost = "$ 2.00";
         //act
         let cost = await index.calculateParkCost(parkType, startDate, leavingDate, startTime, leavingTime);
         //assert
         assert.equal(cost, expectedCost);
     });
 
-    it("Test 2 hours and half cost is $3.00", async ()=> {
+    it("Test 1 hour and half cost is $3.00", async ()=> {
         // arrange
         let parkType = "Short";
         let startDate = "11/14/2020";
-        let leavingDate = "11/16/2020";
+        let leavingDate = "11/14/2020";
         let startTime = "0:00";
-        let leavingTime = "02:30";
-        let expectedCost = "$ 5.00";
-        //act
-        let cost = await index.calculateParkCost(parkType, startDate, leavingDate, startTime, leavingTime);
-        //assert
-        assert.equal(cost, expectedCost);
-    });
-
-    it("Test 2 days and 5 hours cost is $36.00", async ()=> {
-        // arrange
-        let parkType = "Short";
-        let startDate = "11/14/2020";
-        let leavingDate = "11/16/2020";
-        let startTime = "0:00";
-        let leavingTime = "02:30";
+        let leavingTime = "01:30";
         let expectedCost = "$ 3.00";
+        //act
+        let cost = await index.calculateParkCost(parkType, startDate, leavingDate, startTime, leavingTime);
+        //assert
+        assert.equal(cost, expectedCost);
+    });
+
+    it("Test 22 hours cost is $24.00", async ()=> {
+        // arrange
+        let parkType = "Short";
+        let startDate = "11/14/2020";
+        let leavingDate = "11/14/2020";
+        let startTime = "0:00";
+        let leavingTime = "22:30";
+        let expectedCost = "$ 24.00";
+        //act
+        let cost = await index.calculateParkCost(parkType, startDate, leavingDate, startTime, leavingTime);
+        //assert
+        assert.equal(cost, expectedCost);
+    });
+
+    it("Test 2 days cost is $48.00", async ()=> {
+        // arrange
+        let parkType = "Short";
+        let startDate = "11/14/2020";
+        let leavingDate = "11/16/2020";
+        let startTime = "0:00";
+        let leavingTime = "00:0";
+        let expectedCost = "$ 48.00";
         //act
         let cost = await index.calculateParkCost(parkType, startDate, leavingDate, startTime, leavingTime);
         //assert
