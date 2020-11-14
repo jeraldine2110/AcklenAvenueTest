aa84d96b54ad3c098dda1d08f31d7d3d95153e86
Julieth Jeraldine Cordoba Alvear <cordoba@Cordoba-COL.RCC.local>
2020-11-14 17:20:26 -0500

    Test for valet parking type

--- index.js
+++ index.js
@@ -1,76 +1,17 @@
-const {Builder, By, Key, util, JavascriptExecutor} = require("selenium-webdriver");
+const { Builder, By, Key, util, JavascriptExecutor } = require("selenium-webdriver");
 
-async function calculateParkCost(parkType){
+async function calculateParkCost(parkType, startDate, LeavingDate, startingTime, leavingTime) {
     let driver = await new Builder().forBrowser("firefox").build();
-
     await driver.get("http://www.shino.de/parkcalc");
-
-    let originalWindow = await driver.getWindowHandle();    
-    let pickStartDate = driver.findElement(By.xpath("/html/body/form/table/tbody/tr[2]/td[2]/a"));
-    
-    driver.findElement(By.id("ParkingLot")).sendKeys(parkType);
-
-    await pickStartDate.click();
-
-    await driver.wait(
-        async () => (await driver.getAllWindowHandles()).length === 2,
-        2000
-    );
-
-    let windows = await driver.getAllWindowHandles();
-    windows.forEach(async handle => {
-        try {
-            if (handle !== originalWindow) {
-                await driver.switchTo().window(handle);
-                driver.findElement(By.xpath("/html/body/form/table/tbody/tr[5]/td[7]/font/b/a")).click();
-            }
-        } catch (err) {
-            console.log(err);
-        }
-    });    
-
-    await driver.wait(
-        async () => (await driver.getAllWindowHandles()).length === 1,
-        2000
-    );
-
-    driver.switchTo().window(originalWindow);
-    let pickEndDate = driver.findElement(By.xpath("/html/body/form/table/tbody/tr[3]/td[2]/a"));
-    await pickEndDate.click();
-
-    
-    await driver.wait(
-        async () => (await driver.getAllWindowHandles()).length === 2,
-        2000
-    );
-    originalWindow = driver.getWindowHandle();
-    
-    windows = await driver.getAllWindowHandles();
-    windows.forEach(async handle => {
-        try {
-            if (handle !== originalWindow) {
-                await driver.switchTo().window(handle);
-                driver.findElement(By.xpath("/html/body/form/table/tbody/tr[5]/td[7]/font/b/a")).click();
-            }
-        } catch (err) {
-            console.log(err);
-        }
-    });    
-
-    await driver.wait(
-        async () => (await driver.getAllWindowHandles()).length === 1,
-        2000
-    );   
-
-    driver.switchTo().window(originalWindow);
-    driver.executeScript('document.getElementById("StartingTime").value = "11:00"');
-    driver.executeScript('document.getElementById("LeavingTime").value = "13:00"');
+    driver.executeScript("document.getElementById('StartingDate').value='" + startDate + "'");
+    driver.executeScript("document.getElementById('LeavingDate').value='" + LeavingDate + "'");
+    driver.executeScript("document.getElementById('StartingTime').value = '" + startingTime + "'");
+    driver.executeScript("document.getElementById('LeavingTime').value = '" + leavingTime + "'");
     let buttonSubmit = driver.findElement(By.xpath("/html/body/form/input[2]"));
     await buttonSubmit.click();
-
-    let parkCost = await driver.findElement(By.xpath("/html/body/form/table/tbody/tr[4]/td[2]/span[1]/b")).getText()    
+    let parkCost = await driver.findElement(By.xpath("/html/body/form/table/tbody/tr[4]/td[2]/span[1]/b")).getText()
     driver.close();
     return parkCost;
 }
 
-module.exports.calculateParkCost=calculateParkCost;
+module.exports.calculateParkCost = calculateParkCost;
--- test/test_parking.js
+++ test/test_parking.js
@@ -2,19 +2,60 @@ const assert = require("chai").assert;
 var index = require("../index")
 
 describe("Test for valet parking", () => {
-    it("Test two hours cost is $18.00", async ()=> {
+    it("Test 12 hours cost is $18.00", async ()=> {
         // arrange
-        let cost = await index.calculateParkCost("Valet");
+        let parkType = "Valet";
+        let startDate = "11/14/2020";
+        let leavingDate = "11/14/2020";
+        let startTime = "0:00";
+        let leavingTime = "13:00";
+        let expectedCost = "$ 18.00";
         //act
+        let cost = await index.calculateParkCost(parkType, startDate, leavingDate, startTime, leavingTime);
         //assert
-        assert.equal(cost, "$ 12.00");
+        assert.equal(cost, expectedCost);
     });
 
-    it("Test six hours cost is $18.00", ()=> {
+    it("Test 5 hours cost is $12.00", async ()=> {
         // arrange
+        let parkType = "Valet";
+        let startDate = "11/14/2020";
+        let leavingDate = "11/14/2020";
+        let startTime = "0:00";
+        let leavingTime = "05:00";
+        let expectedCost = "$ 12.00";
         //act
+        let cost = await index.calculateParkCost(parkType, startDate, leavingDate, startTime, leavingTime);
         //assert
-        assert.equal(5,5);
+        assert.equal(cost, expectedCost);
+    });
+
+    it("Test 2 days cost is $36.00", async ()=> {
+        // arrange
+        let parkType = "Valet";
+        let startDate = "11/14/2020";
+        let leavingDate = "11/16/2020";
+        let startTime = "0:00";
+        let leavingTime = "00:00";
+        let expectedCost = "$ 36.00";
+        //act
+        let cost = await index.calculateParkCost(parkType, startDate, leavingDate, startTime, leavingTime);
+        //assert
+        assert.equal(cost, expectedCost);
+    });
+
+    it("Test 2 days and 5 hours cost is $36.00", async ()=> {
+        // arrange
+        let parkType = "Valet";
+        let startDate = "11/14/2020";
+        let leavingDate = "11/16/2020";
+        let startTime = "0:00";
+        let leavingTime = "05:00";
+        let expectedCost = "$ 54.00";
+        //act
+        let cost = await index.calculateParkCost(parkType, startDate, leavingDate, startTime, leavingTime);
+        //assert
+        assert.equal(cost, expectedCost);
     });
 });
 
